718013972fc6e48f02d1e00030cfe0ba1f45ff85
*Qpd7kq6p4x <donnebit@gmail.com>
2019-03-18 16:16:46 +0900

    Local DB to Atlas Master DB Transfer Function

--- src/main/java/connection/soup/Helper.java
+++ src/main/java/connection/soup/Helper.java
@@ -4,6 +4,9 @@ import com.google.gson.Gson;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Helper {
 
@@ -21,5 +24,15 @@ public class Helper {
         request.useCookie = false;
 
         return Http.getDocument(request);
+    }
+
+    public static String extractFirstMatch(String pattern, String text) {
+        Matcher matcher = Pattern.compile(pattern).matcher(text);
+        if (matcher.find()) return matcher.group(0);
+        else return "";
+    }
+
+    public static String extractFirstEmail(String text) {
+        return extractFirstMatch("[\\w\\d\\.]+@[\\w\\d\\-\\.]+(\\.[\\w\\d\\-\\.]+)+", text);
     }
 }
--- src/main/java/main/ParserMain.java
+++ src/main/java/main/ParserMain.java
@@ -2,34 +2,64 @@ package main;
 
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.FindOneAndReplaceOptions;
 import connection.ConnectionManager;
 import connection.models.Information;
-import org.bson.Document;
+
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Updates.combine;
 
 public class ParserMain {
 
     public ParserMain() {
-        MongoCollection<Information> collection_to_get = ConnectionManager.getLocalDB().getCollection("information", Information.class);
-        MongoCollection<Information> collection_to_upload = ConnectionManager.getAtlasDB().getCollection("information", Information.class);
+        try {
+            MongoCollection<Information> collection_to_get = ConnectionManager.getLocalDB().getCollection("information", Information.class);
+            MongoCollection<Information> collection_to_upload = ConnectionManager.getAtlasDB().getCollection("information", Information.class);
 
-        FindIterable<Information> info_list = collection_to_get.find();
+            FindIterable<Information> info_list = collection_to_get.find();
 
 
-        int x = 0;
-        for(Information info : info_list) {
-            String mailAddress = (String)info.getData().get("mailAddress");
-            String homepage = (String)info.getData().get("homepage");
+            int data_counter = 0;
+            for (Information info : info_list) {
+                String mailAddress = (String) info.getData().get("mailAddress");
+                String homepage = (String) info.getData().get("homepage");
 
-            if(mailAddress.equals("") && !homepage.equals("")) {
-                //run Scanner here
+                //DISABLED FOR NOW
+                /*
+                Set<String> xx = new HashSet();
 
-                System.out.println(mailAddress + " / " + homepage);
+                if(mailAddress.equals("") && !homepage.equals("")) {
+                    //run Scanner here
+                    Scanner pageScanner = new Scanner();
+                    pageScanner.scanDomain(homepage, 0);
+
+                    for (String links : pageScanner.getList()) {
+                        xx.add(links);
+                        //System.out.println("Link: "+links);
+                    }
+
+                    System.out.println(mailAddress + " / " + homepage);
+                    break;
+
+                }
+
+                for(String xs : xx) System.out.println(xs);*/
+
+
+                System.out.println("UPLOADING TO SERVER: " + info.getData());
+                collection_to_upload.findOneAndReplace(combine(
+                        eq("source", info.getSource()),
+                        eq("site", info.getSite())
+                        ),
+                        info,
+                        new FindOneAndReplaceOptions().upsert(true));
+
+                data_counter++;
             }
-
-            x++;
-            if(x > 30) break;
+            System.out.println("UPLOAD SUCCESSFUL! " + data_counter + " DATA UPLOADED! ");
+        } catch(Exception exc) {
+            System.out.println("ERROR ENCOUNTERED! "+exc.toString());
         }
-
     }
 
     public static void main(String[] args) {
